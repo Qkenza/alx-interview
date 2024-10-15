46cd8724307e97c1f45481d1a2e4a6240759a573
Qkenza <kmerouar@gmail.com>
2024-10-15 15:14:03 -0700

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" 0-minoperations """
+
+
+def minOperations(n):
+    """
+    Calculates the fewest number of operations needed to result
+    in exactly n H characters in the file
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        if n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        else:
+            divisor += 1
+
+    return operations
